233f8b6ef3ed511ddaaea725f390020b081826ae
Lucas <lucasmag97@gmail.com>
2022-10-30 15:56:34 -0300

    [BUG] fix containers creation and initialization

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,20 +7,15 @@ const dataSource = new DataSource({
     type: 'postgres',
     host: process.env.POSTGRES_HOST || 'localhost',
     port: parseInt(process.env.POSTGRES_PORT) || 5432,
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    ssl: true,
+    database: process.env.POSTGRES_DB,
+    ssl: false,
     synchronize: true,
     logging: false,
     entities: ['src/database/entities/*.entity.ts'],
     migrations: ['src/database/migrations/*.ts'],
     subscribers: [],
-    extra: {
-        ssl: {
-            rejectUnauthorized: false,
-        }
-    },
 })
 
 const createConnection = (host = 'postgres'): Promise<DataSource> => {
